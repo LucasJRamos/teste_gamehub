5578ec1b050d1d531d5cc8118dab313ffd87dc87
Murilo Ramos <muriloramos1902@gmail.com>
2026-06-04 14:44:56 -0300

    Adiciona quality gate do Game Hub

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,9 +24,10 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'username' => fake()->unique()->userName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
+            'data_nascimento' => fake()->dateTimeBetween('-35 years', '-16 years')->format('Y-m-d'),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
